e9bcfc09e4fb6d659b22886547097a673e9f0c60
DilshanHarendra <harendracripto@gmail.com>
2022-05-22 23:45:28 +0530

    modify default layout

--- src/layout/DefaultLayout.js
+++ src/layout/DefaultLayout.js
@@ -1,20 +1,29 @@
 import React from 'react'
 import Header from "./defaultLayout/Header";
 import Sidebar from "./defaultLayout/Sidebar";
+import Footer from "./defaultLayout/Footer";
 
 
 function DefaultLayout(props){
-    return(<div className="w-full container mx-auto">
+
+
+
+
+    return(<div  className="w-full  max-h-screen overflow-y-auto relative">
 
         <Header/>
-       <div className="w-full  flex items-center">
-           <div className="w-1/5">
+       <div  className="w-full  mt-24 container mx-auto  flex items-start ">
+           <div className="md:block hidden md:w-1/5">
                <Sidebar/>
            </div>
-           <div className="w-4/5">
+           <div  className="w-full md:w-4/5">
                {props.children}
           </div>
        </div>
+
+        <div>
+            <Footer/>
+        </div>
 
     </div>)
 }export default DefaultLayout
